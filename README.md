36a52c81532f7914c69f2fc4eeff01f40083a5d9
Muhammad Usama <37137366+HMUsama@users.noreply.github.com>
2019-07-13 09:36:11 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Node.js-Express-From-Scratch
+## Node.js-Express-From-Scratch
+
+### Pug
+Pug is a template engine for Node.js. A template engine allows us to inject data and then produce HTML. In short: At run time, Pug (and other template engines) replace variables in our file with actual values, and then send the resulting HTML string to the client.
--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 const express = require('express');
+const path = require('path');
 
+// init app
 const app = express();
 
+//load View Engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+
+// Home Route
 app.get('/',function(req,res){
-    res.send('Hello Usama');
+    let articles = [
+        {
+            id:1,
+            title:'ArticalsOne',
+            auther:'Usama Raza',
+            body:'this is articals One'
+        },
+        {
+            id:2,
+            title:'Articals Two',
+            auther:'Usama Ansari',
+            body:'this is articals Two'
+        },
+        {
+            id:3,
+            title:'Articals Three',
+            auther:'HM Raza',
+            body:'this is articals Three'
+        }
+    ];
+    res.render('index',{
+        title:'Hello usama',
+        articles : articles
+    });
 })
+// Add Route
+app.get('/articals/add',function(req,res){
+    res.render('add_Articals',{
+        title:'Add Articals'
+    })
+})
+
 
 app.listen(3000,function(){
     console.log("Server start")
